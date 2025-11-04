6c2a1d9e4c8bed8d89c93b91610d3f7e41f7fbc2
Bruno Henrique <bruno.lima@sptech.school>
2025-11-03 21:13:08 -0300

    update raw_to_trusted

--- lambda/trigger_raw_to_trusted.py
+++ lambda/trigger_raw_to_trusted.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 s3 = boto3.client('s3')
 TRUSTED_BUCKET = 'trusted-bucket-891377383993'
+CLIENT_BUCKET = 'client-bucket-891377383993'
 
 def lambda_handler(event, context):
     for record in event['Records']:
@@ -15,65 +16,97 @@ def lambda_handler(event, context):
         print(f"Processando arquivo: s3://{source_bucket}/{key}")
 
         try:
-            # Lê o JSON do bucket de origem
-            response = s3.get_object(Bucket=source_bucket, Key=key)
-            raw_bytes = response['Body'].read()
+            raw_to_trusted(source_bucket, key)
+            trusted_to_client(key)
+        except Exception as e:
+            print(f"function=lambda_handler_error file={key} message={e}")
 
-            try:
-                content = raw_bytes.decode('utf-8')
-            except UnicodeDecodeError:
-                content = raw_bytes.decode('latin1')
 
-            # Converte o conteúdo para JSON
-            data = json.loads(content)
-            if isinstance(data, dict):
-                data = [data]
+def raw_to_trusted(source_bucket, key):
+    try:
+        # Lê o JSON do bucket de origem
+        response = s3.get_object(Bucket=source_bucket, Key=key)
+        raw_bytes = response['Body'].read()
 
-            valid_rows = []
-            for row in data:
-                if not row:
-                    continue
-                fk = row.get("fk_sensor")
-                valor = row.get("valor")
-                data_captura = row.get("data_captura")
-                if fk is not None and valor is not None and data_captura and valor != 0:
-                    try:
-                        data_dt = datetime.strptime(data_captura, "%Y-%m-%d %H:%M:%S")
-                        data_str = data_dt.strftime("%Y-%m-%d %H:%M")
-                    except ValueError:
-                        print(f"Formato de data inválido em {data_captura}, arquivo: {key}")
-                        continue
+        # Tenta decodificar o conteúdo
+        try:
+            content = raw_bytes.decode('utf-8')
+        except UnicodeDecodeError:
+            content = raw_bytes.decode('latin1')
 
-                    valid_rows.append({
-                        "fk_sensor": fk,
-                        "valor": valor,
-                        "data_captura": data_str
-                    })
+        # Converte o conteúdo para JSON
+        data = json.loads(content)
+        if isinstance(data, dict):
+            data = [data]
 
-            if not valid_rows:
-                print(f"Nenhum dado válido encontrado no arquivo {key}.")
+        valid_rows = []
+        for row in data:
+            if not row:
                 continue
 
-            # Cria o CSV em memória
-            output = io.StringIO()
-            writer = csv.DictWriter(output, fieldnames=["fk_sensor", "valor", "data_captura"])
-            writer.writeheader()
-            writer.writerows(valid_rows)
-            csv_data = output.getvalue().encode('utf-8')
-            output.close()
+            # Captura campos
+            sensor_1 = row.get("sensor_1")
+            sensor_2 = row.get("sensor_2")
+            sensor_3 = row.get("sensor_3")
+            sensor_4 = row.get("sensor_4")
+            sensor_5 = row.get("sensor_5")
+            sensor_6 = row.get("sensor_6")
+            data_captura = row.get("data_captura")
 
-            # Define o nome do arquivo no trusted-bucket
-            csv_key = key.replace('.json', '.csv')
+            # Validação básica
+            if data_captura:
+                try:
+                    data_dt = datetime.strptime(data_captura, "%Y-%m-%d %H:%M:%S")
+                    data_str = data_dt.strftime("%Y-%m-%d %H:%M")
+                except ValueError:
+                    print(f"Formato de data inválido em {data_captura}, arquivo: {key}")
+                    continue
 
-            # Salva o CSV no bucket confiável
-            s3.put_object(
-                Bucket=TRUSTED_BUCKET,
-                Key=csv_key,
-                Body=csv_data,
-                ContentType='text/csv'
-            )
+                valid_rows.append({
+                    "sensor_1": sensor_1,
+                    "sensor_2": sensor_2,
+                    "sensor_3": sensor_3,
+                    "sensor_4": sensor_4,
+                    "sensor_5": sensor_5,
+                    "sensor_6": sensor_6,
+                    "data_captura": data_str
+                })
 
-            print(f"CSV salvo no bucket '{TRUSTED_BUCKET}' como '{csv_key}'")
+        if not valid_rows:
+            print(f"Nenhum dado válido encontrado no arquivo {key}.")
+            return
 
-        except Exception as e:
-            print(f"Erro ao processar o arquivo {key} do bucket {source_bucket}: {e}")
+        # Cria o CSV em memória
+        output = io.StringIO()
+        writer = csv.DictWriter(output, fieldnames=[
+            "sensor_1",
+            "sensor_2",
+            "sensor_3",
+            "sensor_4",
+            "sensor_5",
+            "sensor_6",
+            "data_captura"
+        ])
+        writer.writeheader()
+        writer.writerows(valid_rows)
+        csv_data = output.getvalue().encode('utf-8')
+        output.close()
+
+        # Define o nome do arquivo no trusted-bucket
+        csv_key = key.replace('.json', '.csv')
+
+        # Salva o CSV no bucket confiável
+        s3.put_object(
+            Bucket=TRUSTED_BUCKET,
+            Key=csv_key,
+            Body=csv_data,
+            ContentType='text/csv'
+        )
+
+        print(f"CSV salvo no bucket '{TRUSTED_BUCKET}' como '{csv_key}'")
+
+    except Exception as e:
+        print(f"function=raw_to_trusted_error file={key} message={e}")
+
+def trusted_to_client(key):
+
